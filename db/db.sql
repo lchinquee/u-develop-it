7a3dd375cf11eb1a0f4a063406d80423436e0240
LChinQuee <81491306+lchinquee@users.noreply.github.com>
2021-07-09 20:21:41 -0400

    add db files

---
+++ db/db.sql
@@ -0,0 +1,4 @@
+DROP DATABASE IF EXISTS election; /*Don't usually include in field because could accidentally delete a coworker's database if titled the same thing*/
+
+CREATE DATABASE election;
+USE election;
